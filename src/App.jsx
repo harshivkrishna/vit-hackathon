199f6b8ef58379a76dfea26f140eec627f671574
harshivkrishna <harshivkrishnam.cse2023@citchennai.net>
2025-01-20 21:44:56 +0530

    modified:   src/App.jsx 	modified:   src/index.css

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,22 @@ import Profile from './components/Profile';
 import Leaderboard from './components/Leaderboard';
 import TreeDensity from './components/TreeDensity';
 import Weather from './components/Weather';
+import './index.css'
+
 function App() {
   return (
-      <div className="min-h-screen bg-gray-50">
+      <div className="min-h-screen">
         <Navbar />
         <main className="max-w-7xl mx-auto py-6 px-4">
           <Routes>
             <Route path="/" element={
-              <div className="text-center py-20">
-                <h1 className="text-4xl font-bold text-green-800 mb-4">Welcome to PlantUp AI</h1>
-                <p className="text-xl text-gray-600 mb-8">Join our community in making the world greener, one tree at a time.</p>
+              <div className=" main-container text-center py-20">
+                <h1 className="main-text text-4xl font-bold text-green-700 mb-4">Welcome to PlantUp AI</h1>
+                <p className="text-xl text-white mb-8">Join our community in making the world greener, one tree at a time.</p>
                 <img 
                   src="https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixlib=rb-4.0.3&auto=format&fit=crop&w=1200&q=80"
                   alt="Green Environment"
-                  className="rounded-lg shadow-lg mx-auto mb-8"
+                  className="rounded-lg w-full main-bg-img left-0 shadow-lg mx-auto mb-8 absolute top-0 -z-10"
                 />
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-4xl mx-auto">
                   <div className="bg-white p-6 rounded-lg shadow">
